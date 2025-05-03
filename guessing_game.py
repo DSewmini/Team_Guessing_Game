304551329b5adbbbc26968dcc83b166c9e6dfc44
UmashaWijewickrama <umshsara2019@gmail.com>
2025-05-03 20:31:01 +0530

    Fix conflict : using range 1 - 75

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,8 @@
 import random 
 while True:
 
-number = random.randint(1, 50) 
-print("Guess a number between 1 and 100") 
+number = random.randint(1, 75) 
+print("Guess a number between 1 and 75") 
 guess = int(input()) 
 if guess < number:
 print("Too low!")
