0992b4d0698e982beebe769de3b5172e0aa6631f
DSewmini <dakshinasewminiarambepola@gmail.com>
2025-05-03 20:56:54 +0530

    add starter code

--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,25 @@
-import random 
+import random
+import time 
 
-number = random.randint(1, 10) 
-print("Guess a number between 1 and 10") 
-guess = int(input()) 
+while True:
+    start_time = time.time()  
+    number = random.randint(1, 75) 
+    print("Guess a number between 1 and 75") 
+    guess = int(input()) 
 
-if guess == number: 
-    print("You win!") 
-else: 
-    print(f"Wrong! The number was {number}")
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+
+
+    if guess == number: 
+        print("You win!") 
+    else: 
+        print(f"Wrong! The number was {number}")
+        
+    print("Play again?(y/n)")
+    if input().lower()!='y':
+        break
+    
+    print(f"Time taken: {time.time() - start_time:.2f}s")
