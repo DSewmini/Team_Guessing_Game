6962d18adc11d840ae892deaf492107018c36e63
vindichamathsara <41-bit-0083@kdu.ac.lk>
2025-05-03 20:24:32 +0530

    replay game

--- guessing_game.py
+++ guessing_game.py
@@ -1,4 +1,5 @@
 import random 
+while True:
 
 number = random.randint(1, 100) 
 print("Guess a number between 1 and 100") 
@@ -13,3 +14,8 @@ if guess == number:
     print("You win!") 
 else: 
     print(f"Wrong! The number was {number}")
+    
+print("Play again?(y/n)")
+if input().lower()!='y':
+    break
+
